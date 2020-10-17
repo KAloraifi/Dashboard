024dc94f54af17e8e2d30bc1a8054a3f8931fd36
Khaled Aloraifi <aloraifi@Khaleds-MacBook-Pro.local>
2020-10-18 00:52:28 +0300

    enable cors without options

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ var corsOptions = {
   }
 }
 
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
