95d39dda994abe71d4c75cf85ecc3f343c9022b3
Khaled Aloraifi <aloraifi@Khaleds-MacBook-Pro.local>
2020-10-18 22:13:19 +0300

    add repo link

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -4,8 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 import MenuIcon from '@material-ui/icons/Menu';
 import LaunchIcon from '@material-ui/icons/Launch';
+
+import githubLogo from '../../assets/GitHub-Mark/github.png'
+
 
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: theme.mixins.toolbar,
@@ -33,6 +37,11 @@ export default function NavigationBar() {
           <Typography variant="h6" className={classes.title}>
             Dashboard
           </Typography>
+          <Tooltip title="Github Repository">
+            <IconButton color="inherit" aria-label="launch" target="_blank" href="https://github.com/KAloraifi/Dashboard" toolt>
+              <img src={githubLogo} />
+            </IconButton>
+          </Tooltip>
           <IconButton color="inherit" aria-label="launch" target="_blank" href="https://www.goodreads.com/api">
             <LaunchIcon />
             <Typography>
